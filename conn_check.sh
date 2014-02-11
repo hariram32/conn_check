6397687a9639dbfbe4345835a6fa63df55d51b68
jcastp <jcastp@gmail.com>
2014-02-11 12:39:25 +0100

    Added command line options

--- conn_check.sh
+++ conn_check.sh
@@ -14,18 +14,64 @@
 # Definition of the script's usage
 usage() {
 	echo "Usage: ${0} [-t|-h]"
-	echo -e "\t -t: If you want to redirect the script to a file, this option will remove the special characters (no colors)."
 	echo -e "\t -h: This help."
+	echo -e "\t -t: If you want to redirect the script to a file, this option will remove the special characters (no colors)."
+	echo -e "\t -l: Shows the last lines of the log, to see if the connector is working properly."
+	echo -e "\t -e: Shows the connector's last EPS rate."
+	echo -e "\t -w: Shows the last 20 errors in the log."
+	echo -e "\t -r: Shows any log line indicating if the logs have been rotated."
+	echo -e "\t -a: Uses all the above options."
 }
 
-# Check number of arguments
-if [ $# -gt 1 ] || [ "$1" == "-h" ]; then
-	usage
-	exit 1
-fi
+# Variable definitions
+text=1 # Use colors
+logs=0 # check the last lines of the logs
+eps=0 # check connectors' eps
+errors=0 # Check errors on log
+rotated=0 # check if the logs are rotated
+
+
+# Parse all the arguments provided
+while getopts "tlewrah" arg;
+do
+    case ${arg} in
+	h)
+	    usage
+	    exit 1
+	    ;;
+	t)
+	    text=0
+	    ;;
+	l)
+	    logs=1
+	    ;;
+	e)
+	    eps=1
+	    ;;
+	w)
+	    errors=1
+	    ;;
+	r)
+	    rotated=1
+	    ;;
+	a)
+	    logs=1
+	    eps=1
+	    errors=1
+	    rotated=1
+	    ;;
+	*)
+	    echo There is an invalid option
+	    echo
+	    usage
+	    exit 1
+	    ;;
+    esac
+done
+
 
 # Color variable definitions
-if [ "$1" == "-t" ]; then
+if [ "${text}" -eq 0 ]; then
 	RED=""
 	GREEN=""
 	YELLOW=""
@@ -58,7 +104,7 @@ do
     echo -e "${YELLOW}*********************************************************************************${CLEAR}"
     #Show name of the connector installed
     conn_name=`basename $c`
-    echo -e "\t${GREEN}--> Connector Name: $conn_name <--${CLEAR}"
+    echo -e "\t${GREEN}*** --> Connector Name: $conn_name <-- ***${CLEAR}"
     
     # Check if the pid file is present
     pid_file=`ls ${CONN_DIR}/${conn_name}/${PID_DIR}/*.pid 2>/dev/null |grep -v java`
@@ -68,7 +114,7 @@ do
     
     # If the pid file is not found, then there is something wrong
     # with the connector
-    if [[ ! -f $pid_file ]]; then
+    if [ ! -f $pid_file ]; then
         echo -e "\t\t${RED}*** [ERROR] We cannot find the PID file for the connector!!! ***${CLEAR}"
         echo -e "\t\t${RED}*** Please, check connector status!!! ***${CLEAR}"
         echo
@@ -87,7 +133,7 @@ do
         ps_rc=$?
         #If process is not running with that PID, or it is assigned to another process,
         # throw an error, and check the next connector.
-        if [[ $ps_rc != 0 ]];then
+        if [ $ps_rc != 0 ];then
             echo -e "\t\t${RED}*** [ERROR] Process not found. ***${CLEAR}"
             echo -e "\t\t${RED}*** Please, check connector status. ***${CLEAR}"
             echo
@@ -98,25 +144,35 @@ do
         echo -e "\t* ps output for that PID is:"
         echo -e "\t\t ${ps_out}"
         echo
+
+	if [ ${eps} -eq 1 ]; then
                 # Check the Eps throughput
-        echo 
-        echo -e "\t* ${GREEN}Last EPS count${CLEAR}"
-        grep "{Eps" -A +1 ${CONN_DIR}/${conn_name}/${LOG_DIR}/${LOG_FILE} | tail -2 
+            echo 
+            echo -e "\t* ${GREEN}Last EPS count${CLEAR}"
+            grep "{Eps" -A +1 ${CONN_DIR}/${conn_name}/${LOG_DIR}/${LOG_FILE} | tail -2 
         echo
+	fi
+
+	if [ ${logs} -eq 1 ] ; then
         # Check the logs
-        echo -e "\t* Last 20 lines from ${CONN_DIR}/${conn_name}/${LOG_DIR}/${LOG_FILE}:"
-        tail -20 ${CONN_DIR}/${conn_name}/${LOG_DIR}/${LOG_FILE}
+            echo -e "\t* Last 20 lines from ${CONN_DIR}/${conn_name}/${LOG_DIR}/${LOG_FILE}:"
+            tail -20 ${CONN_DIR}/${conn_name}/${LOG_DIR}/${LOG_FILE}
+	fi
 
+	if [ ${errors} -eq 1 ]; then
         # Check errors in the log file
-        echo 
-        echo -e "\t* Errors in the log file"
-        grep -i error ${CONN_DIR}/${conn_name}/${LOG_DIR}/${LOG_FILE}
-	echo
+            echo 
+            echo -e "\t* Errors in the log file"
+            grep -i error ${CONN_DIR}/${conn_name}/${LOG_DIR}/${LOG_FILE}
+	    echo
+	fi
 
+	if [ ${rotated} -eq 1 ]; then
 	# Check if there are logs rotated
-	echo -e "\t* Are there any logs rotated?"
-	grep -i rotated ${CONN_DIR}/${conn_name}/${LOG_DIR}/${LOG_FILE} | tail -10
-	echo
+            echo -e "\t* Are there any logs rotated?"
+	    grep -i rotated ${CONN_DIR}/${conn_name}/${LOG_DIR}/${LOG_FILE} | tail -10
+	    echo
+	fi
     fi        
     
 done
